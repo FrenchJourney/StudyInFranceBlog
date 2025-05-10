c98e1cec2902e493421b9c35d3524657cb3a1e5d
FrenchJourney <pgyy165@yahoo.co.jp>
2025-05-10 12:41:30 +0900

    Add files via upload

--- js/jquery.inview_set.js
+++ js/jquery.inview_set.js
@@ -25,14 +25,3 @@ $('.transform3').on('inview', function() {
 	$(this).addClass('transform3style');
 });
 
-//blurスタイルが画面内にきたら、スタイルblurstyleを適用する
-/*
-$('.blur').on('inview', function() {
-	$(this).addClass('blurstyle');
-});
-*/
-
-//fadeスタイルが画面内にきたら、スタイルfade-inを適用する
-/*$('.fade').on('inview', function() {
-	$(this).addClass('fade-in');
-});*/
